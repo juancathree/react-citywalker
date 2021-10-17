bd1c5c545dd160e4279ca5bd1463d2574bd6d5db
juancathree <juancarloslucenamonje@gmail.com>
2021-10-17 09:59:05 +0100

    add styles and i18next provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import global_es from 'translations/es/global.json';
+import global_en from 'translations/en/global.json';
+import 'styles/normalize.css';
+import 'styles/_colors.scss';
+import 'styles/font-family.scss';
+
+i18next.use(LanguageDetector).init({
+   interpolation: { escapeValue: false },
+   detection: {
+      order: ['navigator'],
+      caches: ['cookie'],
+   },
+   fallbackLng: 'en',
+   resources: {
+      es: {
+         global: global_es,
+      },
+      en: {
+         global: global_en,
+      },
+   },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+   <React.StrictMode>
+      <I18nextProvider i18n={i18next}>
+         <App />
+      </I18nextProvider>
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
