0803890e41fad16240c87f8e8d2637ba24cc4185
juancathree <juancarloslucenamonje@gmail.com>
2021-10-17 16:21:41 +0100

    agregate contexts and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import AppRouter from 'routers/AppRouter';
+import { IKContext } from 'imagekitio-react';
+import { ToastContainer } from 'react-toastify';
+import Container from 'components/Container/Container';
+import 'styles/toast.scss';
+
 function App() {
-   return <h2>CityWalker</h2>;
+   return (
+      <IKContext urlEndpoint={process.env.REACT_APP_IK}>
+         <Container>
+            <AppRouter />
+            <ToastContainer
+               position="top-center"
+               autoClose={2000}
+               hideProgressBar={false}
+               newestOnTop={false}
+               closeOnClick
+               rtl={false}
+               pauseOnFocusLoss
+               draggable
+               pauseOnHover={false}
+               className="toast"
+            />
+         </Container>
+      </IKContext>
+   );
 }
 
 export default App;
