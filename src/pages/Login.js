800bcd66819a4b5d5c5c795de253d81fd610a5a2
juancathree <juancarloslucenamonje@gmail.com>
2021-10-17 10:00:14 +0100

    create login page

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import LoginForm from 'components/LoginForm/LoginForm';
+import Container from 'components/Container/Container';
+
+function Login() {
+   return (
+      <Container>
+         <LoginForm />
+      </Container>
+   );
+}
+
+export default Login;
