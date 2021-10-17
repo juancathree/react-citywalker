130ac8efbd223c2b5324b051e01ac0f18c4a4b89
juancathree <juancarloslucenamonje@gmail.com>
2021-10-17 16:22:01 +0100

    create button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+function Button({ children, ...rest }) {
+   return (
+      <button className="button" {...rest}>
+         {children}
+      </button>
+   );
+}
+
+export default Button;
