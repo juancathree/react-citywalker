43baac27471700d4f4c63492b748641a1c0a512a
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:22:05 +0000

    defining types

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,9 +1,11 @@
+import makeActionCreator from './makeActionCreator';
+
 const checkCookie = () => {
    const re = new RegExp(`(?<=citywalkerJWT=)[^;]*`);
    try {
       return document.cookie.match(re)[0];
    } catch {
-      return null;
+      return '';
    }
 };
 
@@ -11,13 +13,16 @@ const authReducer = (state = checkCookie(), action) => {
    switch (action.type) {
       case 'auth/fulfilled':
          return action.payload;
-      case 'auth/signup':
-         return state;
       case 'auth/logout':
          return '';
       default:
          return state;
    }
 };
+
+export const setPending = makeActionCreator('auth/pending');
+export const setFulfilled = makeActionCreator('auth/fulfilled', 'payload');
+export const setRejected = makeActionCreator('auth/rejected', 'error');
+export const setLogout = makeActionCreator('auth/logout');
 
 export default authReducer;
