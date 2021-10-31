d3e71ac5e62fcfd27b4f98b66e9821d26a284b59
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:19:22 +0000

    signup request

---
+++ src/services/signup.js
@@ -0,0 +1,33 @@
+import { setFulfilled, setPending, setRejected } from 'reducers/authReducer';
+
+const signup =
+   ({ name, email, password }) =>
+   async (dispatch) => {
+      dispatch(setPending());
+      try {
+         const response = await fetch(`${process.env.REACT_APP_API}/signup`, {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, password }),
+         });
+         if (!response.ok) {
+            throw new Error(response.status);
+         }
+         const data = await response.json();
+         let d = new Date();
+         d.setTime(d.getTime() + 120 * 24 * 60 * 60 * 1000);
+         document.cookie =
+            'citywalkerJWT=' +
+            data['citywalkerJWT'] +
+            ';expires=' +
+            d +
+            ';path=/';
+         dispatch(setFulfilled(data));
+      } catch (e) {
+         dispatch(setRejected(e.message));
+      }
+   };
+
+export default signup;
