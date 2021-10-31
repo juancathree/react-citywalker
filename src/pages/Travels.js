0952169f901ba61b3737cd42c2bc929884288775
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:22:29 +0000

    create new pages

---
+++ src/pages/Travels.js
@@ -0,0 +1,12 @@
+import Navbar from 'components/Navbar/Navbar';
+
+function Travels() {
+   return (
+      <>
+         <h1>Travels</h1>
+         <Navbar />
+      </>
+   );
+}
+
+export default Travels;
---
+++ src/pages/User.js
@@ -0,0 +1,12 @@
+import Navbar from 'components/Navbar/Navbar';
+
+function User() {
+   return (
+      <>
+         <h1>User</h1>
+         <Navbar />
+      </>
+   );
+}
+
+export default User;
