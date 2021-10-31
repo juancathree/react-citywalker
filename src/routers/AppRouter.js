9e2f091e7c25fed783021132a71ca7c437185860
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:20:35 +0000

    add new routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,10 @@ import PrivateRoute from './PrivateRoute';
 const Login = lazy(() => import('pages/Login'));
 const Signup = lazy(() => import('pages/Signup'));
 const Cities = lazy(() => import('pages/Cities'));
+const City = lazy(() => import('pages/City'));
+const Travels = lazy(() => import('pages/Travels'));
+const User = lazy(() => import('pages/User'));
+const NewTravel = lazy(() => import('pages/NewTravel'));
 
 function AppRouter() {
    return (
@@ -15,6 +19,10 @@ function AppRouter() {
                <Route path="/login" component={Login} exact />
                <Route path="/signup" component={Signup} exact />
                <PrivateRoute path="/cities" component={Cities} exact />
+               <PrivateRoute path="/cities/:id" component={City} exact />
+               <PrivateRoute path="/travels" component={Travels} exact />
+               <PrivateRoute path="/newTravel" component={NewTravel} exact />
+               <PrivateRoute path="/user" component={User} exact />
             </Switch>
          </BrowserRouter>
       </Suspense>
