aa3fa2a60cf6094a2f88317bd9c2568d01e1cf68
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:22:14 +0000

    add reducers

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,11 +1,27 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-import fetchingReducer from './fetchingReducer';
+import citiesReducer from './citiesReducer';
+import placesReducer from './placesReducer';
+import travelsReducer from './travelsReducer';
+import makeAsycTypes from './makeAsyncTypes';
+import makeFetchingReducer from './makeFetchingReducer';
 
 const appReducer = combineReducers({
    auth: combineReducers({
       jwt: authReducer,
-      status: fetchingReducer,
+      status: makeFetchingReducer(makeAsycTypes('auth')),
+   }),
+   cities: combineReducers({
+      data: citiesReducer,
+      status: makeFetchingReducer(makeAsycTypes('cities')),
+   }),
+   places: combineReducers({
+      data: placesReducer,
+      status: makeFetchingReducer(makeAsycTypes('places')),
+   }),
+   travels: combineReducers({
+      data: travelsReducer,
+      status: makeFetchingReducer(makeAsycTypes('travel')),
    }),
 });
 
