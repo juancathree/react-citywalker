711e97ceb82908b6919c9181f8e6aec2173ce853
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:27:12 +0000

    improve inputform

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,20 +1,27 @@
 import { useField } from 'formik';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTranslation } from 'react-i18next';
 import './styles.scss';
 
 function FormInput({ icon, placeholder, type, ...props }) {
    const [field, meta] = useField(props);
+   const [t] = useTranslation('global');
 
    return (
-      <div className={meta.touched && meta.error ? 'input error' : 'input'}>
-         <FontAwesomeIcon className="input__icon" icon={icon} />
-         <input
-            {...field}
-            placeholder={placeholder}
-            type={type}
-            className="input__field"
-         />
-      </div>
+      <>
+         <div className={meta.touched && meta.error ? 'input error' : 'input'}>
+            <FontAwesomeIcon role="icon" className="input__icon" icon={icon} />
+            <input
+               {...field}
+               placeholder={placeholder}
+               type={type}
+               className="input__field"
+            />
+         </div>
+         {meta.touched && meta.error ? (
+            <div className="message__error">{t('auth.error' + field.name)}</div>
+         ) : null}
+      </>
    );
 }
 
