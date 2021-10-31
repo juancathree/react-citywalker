a2a9e9944e0d957ae53267f9f8652156e0cca6c6
juancathree <juancarloslucenamonje@gmail.com>
2021-10-31 03:24:53 +0000

    fixing bugs

--- src/components/UserForm/SignupForm.js
+++ src/components/UserForm/SignupForm.js
@@ -3,11 +3,14 @@ import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
 import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import FormInput from 'components/FormInput/FormInput';
 import Button from 'components/Button/Button';
+import signup from 'services/signup';
 import './styles.scss';
 
-function LoginForm() {
+function SignupForm() {
+   const dispatch = useDispatch();
    const [t] = useTranslation('global');
 
    const SignupSchema = Yup.object().shape({
@@ -20,43 +23,58 @@ function LoginForm() {
       <Formik
          initialValues={{ name: '', email: '', password: '' }}
          validationSchema={SignupSchema}
-         validateOnChange={false}
-         validateOnBlur={false}
+         validateOnChange={true}
          onSubmit={(values) => {
-            console.log(values);
+            dispatch(signup(values));
          }}
       >
-         <Form className="form">
-            <h2 className="form__title">
-               <span className="first">City</span>
-               <span className="second">Walker</span>
-            </h2>
-            <FormInput
-               name="name"
-               type="text"
-               placeholder={t('signup.name')}
-               icon={faUser}
-            />
-            <FormInput
-               name="email"
-               type="email"
-               placeholder={t('login.email')}
-               icon={faEnvelope}
-            />
-            <FormInput
-               name="password"
-               type="password"
-               placeholder={t('login.password')}
-               icon={faLock}
-            />
-            <Button type="submit">{t('signup.signup')}</Button>
-            <h2 className="question">
-               {t('signup.login')}{' '}
-               <Link to="/login">{t('signup.loginLink')}</Link>
-            </h2>
-         </Form>
+         {({ values, errors }) => (
+            <Form className="form">
+               <h2 className="form__title">
+                  <span className="first">City</span>
+                  <span className="second">Walker</span>
+               </h2>
+               <FormInput
+                  name="name"
+                  type="text"
+                  placeholder={t('signup.name')}
+                  icon={faUser}
+               />
+               <FormInput
+                  name="email"
+                  type="email"
+                  placeholder={t('login.email')}
+                  icon={faEnvelope}
+               />
+               <FormInput
+                  name="password"
+                  type="password"
+                  placeholder={t('login.password')}
+                  icon={faLock}
+               />
+               <Button
+                  type="submit"
+                  disabled={
+                     values.email &&
+                     values.password &&
+                     values.name &&
+                     !errors.name &&
+                     !errors.password &&
+                     !errors.email
+                        ? false
+                        : true
+                  }
+               >
+                  {t('signup.signup')}
+               </Button>
+               <h2 className="question">
+                  {t('signup.login')}{' '}
+                  <Link to="/login">{t('signup.loginLink')}</Link>
+               </h2>
+            </Form>
+         )}
       </Formik>
    );
 }
 
-export default LoginForm;
+export default SignupForm;
