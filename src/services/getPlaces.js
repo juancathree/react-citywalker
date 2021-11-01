8a01ca795d5bbafd3c1a543f73fc50b5a27ada4a
juancathree <juancarloslucenamonje@gmail.com>
2021-11-01 00:30:41 +0000

    add saving in session storage

--- src/services/getPlaces.js
+++ src/services/getPlaces.js
@@ -3,18 +3,23 @@ import { setFulfilled, setPending, setRejected } from 'reducers/placesReducer';
 const getPlaces = (lng, id) => async (dispatch) => {
    dispatch(setPending());
    try {
-      const response = await fetch(
-         `${process.env.REACT_APP_API}/${lng.split('-')[0]}/place/${id}`,
-         {
-            method: 'GET',
-            credentials: 'include',
+      if (window.sessionStorage.getItem(id)) {
+         dispatch(setFulfilled(JSON.parse(window.sessionStorage.getItem(id))));
+      } else {
+         const response = await fetch(
+            `${process.env.REACT_APP_API}/${lng.split('-')[0]}/place/${id}`,
+            {
+               method: 'GET',
+               credentials: 'include',
+            }
+         );
+         if (!response.ok) {
+            throw new Error(response.status);
          }
-      );
-      if (!response.ok) {
-         throw new Error(response.status);
+         const data = await response.json();
+         window.sessionStorage.setItem(id, JSON.stringify(data['places']));
+         dispatch(setFulfilled(data['places']));
       }
-      const data = await response.json();
-      dispatch(setFulfilled(data['places']));
    } catch (e) {
       dispatch(setRejected(e.message));
    }
